206ca615992f6a05a94eec035ce9b41a0f6e77d6
Aamir Zaidi <aamir.zaidi2000@gmail.com>
2020-01-22 01:43:18 +0530

    Initial Commit

---
+++ Javascript/index.js
@@ -0,0 +1,41 @@
+let menu_icon = $(".menu_icon")[1];
+let list_wrap = $("#list_wrap")[0];
+let dropdown = $(".dropdown")[0];
+let drop_name = $(".drop_name")[0];
+let dropdown_list = $(".dropdown > ul")[0];
+$(document).ready(() => {
+    let index_obj = new index();
+});
+class index {
+    constructor() {
+        this.init();
+        this.menu_items();
+    }
+    init() {
+        menu_icon.addEventListener("click", (e) => {
+            $(list_wrap).toggle();
+        });
+        $(drop_name).on("click", (e) => {
+            $(dropdown_list).slideToggle("slow");
+        });
+    }
+    menu_items() {
+        $("#list_wrap li").on("click", (e) => {
+            $("#list_wrap li, #list_wrap span *, #list_wrap li a").removeClass("active");
+            $(e.target).toggleClass("active");
+            if (e.target.tagName == "A") {
+                $(e.target.parentElement).toggleClass("active");
+            }
+            if ($(e.target).hasClass("drop_name") || $(e.target).hasClass("fa-sort-down")) {
+                $(drop_name.parentElement).toggleClass("active");
+                $(drop_name).removeClass("active");
+                $(".dropdown > ul > li").on("click", (e) => {
+                    e.stopPropagation();
+                    $(".dropdown > ul > li").removeClass("active_drop");
+                    $(e.target).toggleClass("active_drop");
+                    dropdown.classList.add("active");
+                });
+            }
+        });
+    }
+}
